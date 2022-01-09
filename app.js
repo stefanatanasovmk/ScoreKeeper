02e63ad94e0c5031bfde72ab93108f7da5dc3f6e
stefanatanasovmk <74651762+stefanatanasovmk@users.noreply.github.com>
2022-01-09 15:19:48 +0200

    Update 1.1
    Making the code shorter, shortening the functions, adding some async function which gave some cool transitions etc.

--- app.js
+++ app.js
@@ -1,185 +1,225 @@
-let playerCard = document.querySelector(".player1")
 let addPlayer = document.querySelector("#addPlayer")
 let container = document.querySelector(".container")
-let resetbtn = document.querySelector("#reset")
+let resetBtn = document.querySelector("#reset")
+let playerCards = document.getElementsByClassName("playerCard")
 
-
-
-let randomColor = function () {
-     let r = `${Math.floor(Math.random() * (255 - 120)) + 120
+let RGBnumbers = function () {
+     let res = `${Math.floor(Math.random() * (255 - 120)) + 120
           }`
-     let g = `${Math.floor(Math.random() * (255 - 120)) + 120
-          }`
-     let b = `${Math.floor(Math.random() * (255 - 120)) + 120
-          }`
-     return `rgb(${r},${g},${b})`
+     return res
 }
-let randomColorAll = function () {
-     let r = `${Math.floor(Math.random() * 255) + 1
-          }`
-     let g = `${Math.floor(Math.random() * 255) + 1
-          }`
-     let b = `${Math.floor(Math.random() * 255) + 1
-          }`
-     return `rgb(${r},${g},${b})`
+let randomColor = function () {
+     return `rgb(${RGBnumbers()},${RGBnumbers()},${RGBnumbers()})`
 }
 
 let divNum = 0
 
-for (let i = 0; i < 3; i++) {
-     addPlayerCard()
-}
-
-addPlayer.onclick = addPlayerCard
-
 function addPlayerCard(e) {
      divNum++
 
-     let cardSection = document.createElement("section")
-     document.querySelector(".container").appendChild(cardSection)
-     cardSection.classList.add(`cardNum${divNum}`, "cardSection")
-
      let newPlayerCard = document.createElement("div")
-     document.querySelector(`.cardNum${divNum}`).appendChild(newPlayerCard)
-     newPlayerCard.classList.add(`player${divNum}`, "forDelete")
+     document.querySelector(".container").appendChild(newPlayerCard)
+     newPlayerCard.classList.add(`player${divNum}`, "playerCard")
      newPlayerCard.style.background = randomColor()
 
-     let addMoreBtn = document.createElement("input")
-     document.querySelector(`.cardNum${divNum}`).appendChild(addMoreBtn)
-     addMoreBtn.classList.add(`.addMoreBtn${divNum}`, "moreLessPointsBtn")
-     addMoreBtn.placeholder = "Add More Points"
-     addMoreBtn.style.backgroundColor = randomColor()
-     addMoreBtn.maxLength = "3"
-
-     addMoreBtn.addEventListener("keyup", function (e) {
-          let addedPoints = addMoreBtn.value
-          let cardNum = e.target.parentElement.classList[0].slice(7, 10)
-          if (e.key === "Enter" && (isNaN(addedPoints)) || addedPoints.trim() === "") {
-               addMoreBtn.value = ""
-               addMoreBtn.setAttribute("placeholder", "Enter a number")
-          } else if (e.key === "Enter" && !(isNaN(addedPoints))) {
-               let updatedText = document.querySelector(`#scoretxt${cardNum}`)
-               updatedText.innerHTML = numberTxt += parseInt(addedPoints)
-               addMoreBtn.value = ""
-               addMoreBtn.setAttribute("placeholder", "Add More Points")
-
-          }
-
-     })
-
-     let addLessBtn = document.createElement("input")
-     document.querySelector(`.cardNum${divNum}`).appendChild(addLessBtn)
-     addLessBtn.classList.add(`.addLessBtn${divNum}`, "moreLessPointsBtn")
-     addLessBtn.placeholder = "Minus More Points"
-     addLessBtn.style.backgroundColor = randomColor()
-     addLessBtn.maxLength = "3"
-
-     addLessBtn.addEventListener("keyup", function (e) {
-          let minusedPoints = addLessBtn.value
-          let cardNum = e.target.parentElement.classList[0].slice(7, 10)
-          if (e.key === "Enter" && (isNaN(minusedPoints)) || minusedPoints.trim() === "") {
-               addLessBtn.value = ""
-               addLessBtn.setAttribute("placeholder", "Enter a number")
-
-          } else if (e.key === "Enter" && !(isNaN(minusedPoints))) {
-               let updatedText = document.querySelector(`#scoretxt${cardNum}`)
-               updatedText.innerHTML = numberTxt -= parseInt(minusedPoints)
-               addLessBtn.value = ""
-               addLessBtn.setAttribute("placeholder", "Minus More Points")
-
-          }
-     })
-     addLessBtn.onfocus = whenClicked
-     addMoreBtn.onfocus = whenClicked
-     function whenClicked() {
-          this.style.outline = `solid 3px ${randomColorAll()}`
-     }
-     addLessBtn.onblur = whenNotClicked
-     addMoreBtn.onblur = whenNotClicked
-     function whenNotClicked() {
-          this.style.outline = "none"
-     }
-
-     let plusbtn = document.createElement("button")
-     plusbtn.setAttribute("id", `plusbtn${divNum}`)
-     plusbtn.setAttribute("class", "plussbutton")
-     document.querySelector(`.player${divNum}`).appendChild(plusbtn)
+     let plusBtn = document.createElement("button")
+     plusBtn.setAttribute("id", `plusBtn${divNum}`)
+     plusBtn.setAttribute("class", "plussbutton")
+     document.querySelector(`.player${divNum}`).appendChild(plusBtn)
 
      let playerNameInput = document.createElement("input")
      playerNameInput.placeholder = "Player Name"
+     playerNameInput.classList.add(`playerNameClass${divNum}`, "playerNameInput")
      document.querySelector(`.player${divNum}`).appendChild(playerNameInput)
 
-     playerNameInput.addEventListener("keyup", function (e) {
-          if (e.key === "Enter") {
-               let playerName = playerNameInput.value
-               let textAreaCreate = document.createElement("span")
-               textAreaCreate.innerHTML = playerName
-               textAreaCreate.setAttribute("class", "nameSpan")
-               plusbtn.after(textAreaCreate)
-               playerNameInput.remove()
-
-          }
-     });
-
-
-     let minusbtn = document.createElement("button")
-     minusbtn.setAttribute("id", `minusbtn${divNum}`)
-     minusbtn.setAttribute("class", "minusbutton")
-     document.querySelector(`.player${divNum}`).appendChild(minusbtn)
+     let minusBtn = document.createElement("button")
+     minusBtn.setAttribute("id", `minusBtn${divNum}`)
+     minusBtn.setAttribute("class", "minusbutton")
+     document.querySelector(`.player${divNum}`).appendChild(minusBtn)
 
      let scoreText = document.createElement("span")
-     let numberTxt = 0
-     scoreText.innerHTML = numberTxt
+     scoreText.innerHTML = 0
      scoreText.setAttribute("id", `scoretxt${divNum}`)
      scoreText.setAttribute("class", "scoreText")
      document.querySelector(`.player${divNum}`).appendChild(scoreText)
 
-     let deletebtn = document.createElement("button")
-     deletebtn.setAttribute("class", "deletebutton")
-     document.querySelector(`.player${divNum}`).appendChild(deletebtn)
 
+     let deleteBtn = document.createElement("button")
+     deleteBtn.setAttribute("class", "deletebutton")
+     document.querySelector(`.player${divNum}`).appendChild(deleteBtn)
 
-     plusbtn.onclick = whenClickPlus
+     let addMoreBtn = document.createElement("input")
+     document.querySelector(`.player${divNum}`).appendChild(addMoreBtn)
+     addMoreBtn.classList.add(`.addMoreBtn${divNum}`, "moreLessPointsBtn")
+     addMoreBtn.placeholder = "Plus More Points"
+     addMoreBtn.setAttribute("type", "number")
+     addMoreBtn.setAttribute("min", "1")
+     addMoreBtn.setAttribute("max", "999")
+     addMoreBtn.onblur = whenNotClicked
+     addMoreBtn.onfocus = whenClicked
+
+     let addLessBtn = document.createElement("input")
+     document.querySelector(`.player${divNum}`).appendChild(addLessBtn)
+     addLessBtn.classList.add(`.addLessBtn${divNum}`, "moreLessPointsBtn")
+     addLessBtn.placeholder = "Minus More Points"
+     addLessBtn.setAttribute("type", "number")
+     addLessBtn.setAttribute("min", "1")
+     addLessBtn.setAttribute("max", "999")
+     addLessBtn.onblur = whenNotClicked
+     addLessBtn.onfocus = whenClicked
+
+     playerNameInput.addEventListener("keyup", function (e) {
+          let playerName = playerNameInput.value
+          if (e.key === "Enter" && playerName.length > 0) {
+               let textAreaCreate = document.createElement("span")
+               textAreaCreate.innerHTML = playerName
+               textAreaCreate.classList.add("nameSpan", `nameNum${divNum}`)
+               plusBtn.after(textAreaCreate)
+               playerNameInput.remove()
+               if (playerName.length > 0) {
+                    plusBtn.onclick = whenClickPlus
+                    minusBtn.onclick = whenClickMinus
+                    addMoreBtn.onkeyup = whenInputPlus
+                    addLessBtn.onkeyup = whenInputMinus
+               }
+          } else if (e.key === "Enter" && playerName.length === 0) {
+               playerNameInput.placeholder = "Please enter valid name"
+          }
+
+     });
 
      function whenClickPlus(e) {
-          let cardNumber = e.target.parentElement.classList[0].slice(6, 10)
-          let updatedText = document.querySelector(`#scoretxt${cardNumber}`)
-          numberTxt++
-          if (e.target.id === `plusbtn${cardNumber}`) {
-               updatedText.textContent = numberTxt
-          }
+          let cardNum = e.target.parentElement.classList[0].slice(6, 10)
+          let textArea = document.querySelector(`#scoretxt${cardNum}`)
+          let scoreNum = parseInt(textArea.innerHTML)
+          textArea.innerHTML = scoreNum + 1
      }
 
-     deletebtn.onclick = whenClickDelete
+
+     function whenClickMinus(e) {
+          let cardNum = e.target.parentElement.classList[0].slice(6, 10)
+          let textArea = document.querySelector(`#scoretxt${cardNum}`)
+          let scoreNum = parseInt(textArea.innerHTML)
+          textArea.innerHTML = scoreNum - 1
+     }
+
+     deleteBtn.onclick = whenClickDelete
      function whenClickDelete(e) {
           let cardNumber = e.target.parentElement.classList[0].slice(6, 10)
-          let card = document.querySelector(`.cardNum${cardNumber}`)
+          let card = document.querySelector(`.player${cardNumber}`)
           card.remove()
      }
 
-     minusbtn.onclick = whenClickMinus
-     function whenClickMinus(e) {
-          let cardNumber = e.target.parentElement.classList[0].slice(6, 10)
-          let updatedText = document.querySelector(`#scoretxt${cardNumber}`)
-          numberTxt--
-          if (e.target.id === `minusbtn${cardNumber}`) {
-               updatedText.textContent = numberTxt
+     let whenInputPlus = function (e) {
+          e.preventDefault()
+          let addedPoints = addMoreBtn.value
+          let cardNum = e.target.parentElement.classList[0].slice(6, 10)
+          if (e.key === "Enter" && addedPoints < 1000 && addedPoints > 0) {
+               let textArea = document.querySelector(`#scoretxt${cardNum}`)
+               let scoreNum = parseInt(textArea.innerHTML)
+               textArea.innerHTML = scoreNum + parseInt(addedPoints)
+               addMoreBtn.value = ""
+          } else if (e.key === "Enter" && addedPoints >= 1000 || addedPoints < 0) {
+               addMoreBtn.value = ""
+               addMoreBtn.setAttribute("placeholder", "Max 999")
           }
+     }
 
+     let whenInputMinus = function (e) {
+          let minusedPoints = addLessBtn.value
+          let cardNum = e.target.parentElement.classList[0].slice(6, 10)
+          if (e.key === "Enter" && minusedPoints < 1000 && minusedPoints > 0) {
+               let textArea = document.querySelector(`#scoretxt${cardNum}`)
+               let scoreNum = parseInt(textArea.innerHTML)
+               textArea.innerHTML = scoreNum - parseInt(minusedPoints)
+               addLessBtn.value = ""
+          } else if (e.key === "Enter" && minusedPoints >= 1000) {
+               addLessBtn.value = ""
+               addLessBtn.setAttribute("placeholder", "Max 999")
+          }
      }
 
 
-     let allSection = document.querySelectorAll(".cardSection")
-     resetbtn.onclick = whenResetClick
-     function whenResetClick() {
-          for (let a of allSection) {
-               a.remove()
-          }
+     function whenClicked() {
+          this.style.outline = `solid 3px ${randomColor()}`
+     }
 
-          for (let i = 0; i < 3; i++) {
-               addPlayerCard()
-          }
-
+     function whenNotClicked() {
+          this.style.outline = "none"
      }
 
 }
+
+
+function onStart() {
+     return new Promise((resolved) => {
+          setTimeout(() => {
+               addPlayerCard()
+               resolved()
+          }, 150);
+     })
+}
+
+function opacity() {
+     return new Promise((resolved) => {
+          setTimeout(() => {
+               for (let i of playerCards) {
+                    i.classList.add("opacityClass")
+               }
+          }, 20)
+          resolved()
+     })
+}
+
+async function whenOpenPage() {
+     try {
+          await onStart()
+          await opacity()
+          await onStart()
+          await opacity()
+          await onStart()
+          await opacity()
+     }
+     catch (e) {
+          console.log(e)
+     }
+}
+
+whenOpenPage()
+
+async function whenClickAdd() {
+     try {
+          await onStart()
+          await opacity()
+     }
+     catch (e) {
+          console.log(e)
+     }
+}
+
+addPlayer.onclick = whenClickAdd
+
+
+function removeElements() {
+     return new Promise((resolved) => {
+          let playerCards = document.querySelectorAll(".playerCard")
+          for (let i of playerCards) {
+               i.remove()
+          }
+          resolved()
+     })
+
+}
+
+async function whenClickReset() {
+     try {
+          await removeElements()
+          await whenOpenPage()
+     }
+     catch (e) {
+          console.log(e)
+     }
+
+}
+
+resetBtn.onclick = whenClickReset
